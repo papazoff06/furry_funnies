5a624c213ccd6f88b4dca08d13ab72fee3e21b77
papazoff06 <134053962+papazoff06@users.noreply.github.com>
2025-06-09 17:23:47 +0300

    ste upp settings and project urls

--- furry_funnies/urls.py
+++ furry_funnies/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('furry_funnies.urls')),
+    path('author/', include('furry_funnies.urls')),
+    path('posts/', include('furry_funnies.urls')),
 ]
